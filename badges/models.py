c1e90edec18c4525069f6295bec00158460999ad
Brendon Roberto <orph3us01@gmail.com>
2013-08-26 14:42:13 -0400

    Add max_length to CharFields, add upload_to to FileFields and ImageFields

--- badges/models.py
+++ badges/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from profiles.models import UserProfile
 
 class Badge(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     description = models.TextField()
     users_set = models.ManyToManyField(UserProfile)
-    image_data = models.ImageField()
+    image_data = models.ImageField(upload_to='/badges')
--- profiles/models.py
+++ profiles/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+
+def generate_upload_path(instance, filename):
+    return '/'.join('content', instance.project_item.project.profile.username)
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User)
     email = models.EmailField(unique=True)
     username = models.CharField(unique=True, max_length=20)
-    first_name = models.CharField()
-    last_name = models.CharField()
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
 
 class StudentUser(models.Model):
     profile = models.ForeignKey(UserProfile)
@@ -16,25 +20,28 @@ class MentorUser(models.Model):
 
 class Project(models.Model):
     profile = models.ForeignKey(UserProfile)
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     start_date = models.DateField(blank=True)
     end_date = models.DateField(blank=True)
 
 class ProjectItem(models.Model):
     project = models.ForeignKey(Project)
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
 
 class ProjectItemImage(models.Model):
     project_item = models.ForeignKey(ProjectItem)
     url = models.URLField()
-    image_data = models.ImageField()
+    image_data = models.ImageField(upload_to=generate_upload_path)
 
 class ProjectItemFile(models.Model):
     project_item = models.ForeignKey(ProjectItem)
     url = models.URLField()
-    file_data = models.FileField()
+    file_data = models.FileField(upload_to=generate_upload_path)
     
 class Recommendation(models.Model):
     student = models.ForeignKey(StudentUser)
     mentor = models.ForeignKey(MentorUser)
+    subject = models.CharField(max_length=100)
+    body = models.TextField()
