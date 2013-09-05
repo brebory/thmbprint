2a6bea114ed56cdad46997210571bbbc6001780c
Brendon Roberto <orph3us01@gmail.com>
2013-09-05 17:53:38 -0400

    Fix error in settings.py

--- LaunchpadProfiles/settings.py
+++ LaunchpadProfiles/settings.py
@@ -85,7 +85,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, 'assets'),
+    os.path.join(SITE_ROOT, '..', 'assets'),
 )
 
 # List of finder classes that know how to find static files in
