04c163f585c51f3b4e3fe598da8f74b47e65a510
Brendon Roberto <orph3us01@gmail.com>
2013-09-05 17:44:24 -0400

    Fix collectstatic bug with heroku, maybe.

--- LaunchpadProfiles/settings.py
+++ LaunchpadProfiles/settings.py
@@ -77,7 +77,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(SITE_ROOT, '..', 'static')
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -85,7 +85,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(SITE_ROOT, '..', 'assets'),
+    os.path.join(SITE_ROOT, 'assets'),
 )
 
 # List of finder classes that know how to find static files in
