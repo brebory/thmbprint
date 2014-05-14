56ba71ed82bfb1840bef67e3266d862985913cf5
Brendon Roberto <brendon@brendonroberto.com>
2014-05-14 12:38:34 -0400

    Fix csrf protection on badges_list view

--- badges/views.py
+++ badges/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render_to_response
+from django.core.context_processors import csrf
+from django.template import RequestContext
 from badges.models import Badge
 
 def list_badges(request):
@@ -7,7 +9,8 @@ def list_badges(request):
     """
     badges = Badge.objects.all()
     c = { 'badges': badges }
-    return render_to_response('badge_list.jade', c)
+    c.update(csrf(request))
+    return render_to_response('badge_list.jade', c, RequestContext(request))
 
 def badge_detail(request, badge_id):
     """
@@ -15,4 +18,5 @@ def badge_detail(request, badge_id):
     """
     badge = Badge.objects.get(pk=badge_id) 
     c = { 'badge': badge }
-    return render_to_response('badge_detail.jade', c)
+    c.update(csrf(request))
+    return render_to_response('badge_detail.jade', c, RequestContext(request))
