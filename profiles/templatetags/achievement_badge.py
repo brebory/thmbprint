1293e490c1d9d8fac314a7972513ebaea5d845ce
Brendon Roberto <brendon@brendonroberto.com>
2014-05-20 11:50:26 -0400

    Add getbadgeurl to achievement_badge templatetags and add badge images to achievements panel

--- profiles/templatetags/achievement_badge.py
+++ profiles/templatetags/achievement_badge.py
@@ -5,4 +5,8 @@ register = template.Library()
 def getbadgeinstance(badgelist, achievementid):
     return badgelist.get(achievement_key = achievementid).name
 
+def getbadgeurl(badgelist, key):
+    return badgelist.get(achievement_key = key).image_data.url
+
 register.simple_tag(getbadgeinstance)
+register.simple_tag(getbadgeurl)
